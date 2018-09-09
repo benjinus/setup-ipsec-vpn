6dad1dd1caa847ab18c4e7ef8c406dbdfde29bfc
Benjamin Linus <40568853+benjinus@users.noreply.github.com>
2018-09-09 23:24:29 +0800

    Update vpnsetup.sh

--- vpnsetup.sh
+++ vpnsetup.sh
@@ -24,9 +24,9 @@
 # - All values MUST be placed inside 'single quotes'
 # - DO NOT use these special characters within values: \ " '
 
-YOUR_IPSEC_PSK=''
-YOUR_USERNAME=''
-YOUR_PASSWORD=''
+YOUR_IPSEC_PSK='xiasenhai'
+YOUR_USERNAME='xiasenhai'
+YOUR_PASSWORD='xiasenhai123890'
 
 # Important notes:   https://git.io/vpnnotes
 # Setup VPN clients: https://git.io/vpnclients
